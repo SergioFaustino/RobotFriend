0a10e7fbb5dd6b5b6fb51bf737e64cb740ca3ba8
Sergio Faustino <smfalcas@gmail.com>
2018-12-28 18:47:38 +0000

    Initial Robot Friend App

---
+++ src/containers/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import Deck from '../components/Deck';
+import SearchBox from '../components//SearchBox.js';
+import Scroll from '../components/Scroll.js';
+import ErrorBoundry from '../components/ErrorBoundry.js';
+import './App.css'
+
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			robots: [],
+			searchfield: ''
+		}
+	}
+
+	componentDidMount () {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json() )
+		.then (users => this.setState({robots : users}));
+	}
+
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value })
+	}
+
+	render() {
+		const { robots, searchfield } = this.state;
+		const filteredRobots = robots.filter(
+				robot => {return robot.name.toLowerCase().includes(searchfield.toLowerCase());}
+		);
+		return (
+			<div className='tc'>
+				<h1 className='f1'>RobotFriends</h1>
+				<SearchBox searchChange={this.onSearchChange}/>
+				<Scroll>
+					<ErrorBoundry>
+						<Deck robots={filteredRobots}/>
+					</ErrorBoundry>
+				</Scroll>
+			</div>
+		);	
+	}
+}
+
+export default App;
